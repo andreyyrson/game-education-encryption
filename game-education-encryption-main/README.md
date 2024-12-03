12b059dc28aa16f40e1644ef9b0fb49fe2c23478
Felipe Brazão <felipebrazao@outlook.com>
2024-12-03 19:43:13 -0300

    Add files via upload

---
+++ game-education-encryption-main/README.md
@@ -0,0 +1,2 @@
+# game-education-encryption
+CriptoTuring é um jogo educativo em HTML, CSS e JavaScript que ensina criptografia com temática da Segunda Guerra Mundial. Como estagiário de Alan Turing, você resolverá enigmas e aprenderá técnicas de criptografia, aplicando-as em desafios para ajudar a decifrar códigos secretos. Divirta-se e aprenda!
---
+++ game-education-encryption-main/turing/fase1.js
@@ -0,0 +1,51 @@
+
+const substitutionTable = {
+    "A": "D", "B": "E", "C": "F", "D": "G", "E": "H", "F": "I", 
+    "G": "J", "H": "K", "I": "L", "J": "M", "K": "N", "L": "O", 
+    "M": "P", "N": "Q", "O": "R", "P": "S", "Q": "T", "R": "U", 
+    "S": "V", "T": "W", "U": "X", "V": "Y", "W": "Z", "X": "A", 
+    "Y": "B", "Z": "C"
+};
+
+
+function substitutionDecrypt(cipherText) {
+    let decryptedMessage = '';
+    for (let i = 0; i < cipherText.length; i++) {
+        let char = cipherText[i].toUpperCase();
+
+        if (char.match(/[A-Z]/)) {
+          
+            for (let key in substitutionTable) {
+                if (substitutionTable[key] === char) {
+                    decryptedMessage += key;
+                    break;
+                }
+            }
+        } else {
+            decryptedMessage += char; 
+        }
+    }
+    return decryptedMessage;
+}
+
+// Verificar a Resposta
+document.getElementById('verifyAnswerButton').addEventListener('click', () => {
+    const correctAnswer = "Paysandu";
+    const userAnswer = document.getElementById('userAnswer').value.trim();
+
+    if (userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
+        document.getElementById('successMessage').style.display = 'block';
+        document.getElementById('loading').style.display = 'block';
+        setTimeout(() => {
+            window.location.href = 'fase2.html'; 
+        }, 3000); 
+    } else {
+        alert('Resposta incorreta! Tente novamente.');
+    }
+});
+
+
+document.getElementById('helpButton').addEventListener('click', () => {
+    const tipContainer = document.getElementById('tipContainer');
+    tipContainer.style.display = tipContainer.style.display === 'none' ? 'block' : 'none';
+});
