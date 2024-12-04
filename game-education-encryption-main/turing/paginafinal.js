ab489420f595af6802ea1b0a5cd731a2ad5b42b6
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2024-12-04 20:28:23 -0300

    Delete game-education-encryption-main directory

--- game-education-encryption-main/turing/paginafinal.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('continueButton').addEventListener('click', () => {
-    // Redireciona o jogador para a página inicial ou outra página
-    window.location.href = 'index.html'; // Aqui você pode definir para redirecionar para qualquer outra página.
-});
